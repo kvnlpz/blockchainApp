f3865cf41ad8390f0f7a2117eccc8e805bd21df0
Kevin L <kevinlopez8554@csu.fullerton.edu>
2021-10-18 17:08:51 -0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
      * IP Addresses for each validator node
      */
     peers: [
-        '70.93.197.15', // KEVIN
-        '98.147.144.21'  // SHYAM
+        '-', // THIS MACHINE
+        '-'  // OTHER MACHINE
     ]
 }
